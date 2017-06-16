8faa37f950ea14a5ba319b9278fec2df30139912
gpynes <gpynes@gmail.com>
2017-06-16 17:06:31 +0200

    removes log

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -12,7 +12,7 @@ const imageSearch = (queryString, offset) => {
     }
     const query = queryString.split(' ').join('%20')
     offset = offset || 0
-    console.log('Called', query)
+
     return fetch(`https://www.googleapis.com/customsearch/v1?q=${query}&searchType=image&num=10&offset=${offset}&key=${config.custom_search.key}&cx=${config.custom_search.cx}`)
             .then(res => res.json())
             .then(res => res.items)
